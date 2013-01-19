6e8e114187dcee1e593a17ab89a91c20ca534f3a
Marjori Pomarole <marjoripomarole@gmail.com>
2013-01-18 20:19:22 -0800

    stuff

--- halpzme/settings.py
+++ halpzme/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'admin',                      # Or path to database file if using sqlite3.
         'USER': 'admin',                      # Not used with sqlite3.
-        'PASSWORD': 'dady%Ola',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
